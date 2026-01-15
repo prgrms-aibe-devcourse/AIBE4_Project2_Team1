7d7440f94676e5697fb30ba94e82e4ca624b8ff6
20harry00 <yeong0586@gmail.com>
2026-01-15 11:15:30 +0900

    실행파일

--- src/main/java/websocket/Pr1maryApplication.java
+++ src/main/java/websocket/Pr1maryApplication.java
@@ -1,8 +1,9 @@
-package websocket; // 본래 패키지명으로 유지하세요
+package websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Pr1maryApplication {
     public static void main(String[] args) {
         SpringApplication.run(Pr1maryApplication.class, args);
