5103e58f63ccf059a86687525baa02a6d6e642cc
20harry00 <yeong0586@gmail.com>
2026-01-15 11:04:17 +0900

    라이브러리 종속 관계 변경

---
+++ src/main/java/websocket/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,10 @@
+package websocket.chat.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import websocket.domain.ChatRoom;
+
+import java.util.Optional;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    Optional<ChatRoom> findByUserAAndUserB(String userA, String userB);
+}
